96090276b4dc394d0e28fa6b916402780f9c8beb
sina rahimi <sina.1999.rahimi@gmail.com>
2025-05-10 12:05:15 +0330

    chore(i18n): test cardinal pluralization

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -32,6 +32,11 @@ export default function Home({
     <div>
       <h1 className="font-bold text-xl">{t("title", { name: "Sina" })}</h1>
       <h3 className="text-gray-600">{t("description")}</h3>
+      <ul className="my-4 border border-sky-400 border-dashed w-fit list-disc list-inside py-2 px-4 rounded-lg font-medium text-lg text-sky-700">
+        <li>{t("carCount", { count: 0 })}</li>
+        <li>{t("carCount", { count: 1 })}</li>
+        <li>{t("carCount", { count: 2 })}</li>
+      </ul>
       <Link className="mt-4 text-blue-500 hover:text-blue-600" href="/about">
         {aboutPageT("urlTitle")}
       </Link>
